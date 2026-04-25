97902009d398fab257fdb3f60b04d7c282f32ed0
firenza <masudtxg@gmail.com>
2026-04-25 19:20:22 +0700

    feat: implement SUSFS support for kernel filesystem operations and directory iteration

--- kernel/sys.c
+++ kernel/sys.c
@@ -1194,12 +1194,18 @@ static int override_release(char __user *release, size_t len)
 	return ret;
 }
 
+#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
+extern struct static_key_false susfs_is_uname_spoof_buffer_set;
+extern void susfs_spoof_uname(struct new_utsname* tmp);
+#endif
+
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
+
 	if (!strncmp(current->comm, "bpfloader", 9) ||
 	    !strncmp(current->comm, "netbpfload", 10) ||
 	    !strncmp(current->comm, "netd", 4)) {
@@ -1207,6 +1213,12 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 		pr_debug("uname: %s release=%s\n",
 			 current->comm, tmp.release);
 	}
+
+#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
+	if (static_branch_likely(&susfs_is_uname_spoof_buffer_set))
+		susfs_spoof_uname(&tmp);
+#endif
+
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
